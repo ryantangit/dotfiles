307a237690c945fa35405a958296210e589e0063
Ryan Tan <ryantan.mail@gmail.com>
2025-02-06 17:58:49 -0800

    update tab keymapping

--- nvim/lua/config/tabs.lua
+++ nvim/lua/config/tabs.lua
@@ -1,8 +1,20 @@
 
 -- Keymaps for tabs
+
+-- New Tab
 vim.keymap.set('n', '<leader>tt', '<cmd>tabnew<cr>i<Esc><CMD>NvimTreeToggle<cr>', { desc = 'Create New Tab' })
+
+-- Next Tab
 vim.keymap.set('n', '<leader>tn', '<cmd>tabn<cr>', { desc = 'Move to Next Tab '})
+
+-- Previous Tab
 vim.keymap.set('n', '<leader>tp', '<cmd>tabp<cr>', {silent = true, desc = 'Move to Previous Tab '})
+
+-- Select Tab 1 ... 9
 for i = 1, 9 do
     vim.keymap.set('n', '<leader>t' .. i, ':tabnext ' .. i .. '<cr>', { noremap = true, silent = true })
 end
+
+-- Close Current Tab
+vim.keymap.set('n', '<leader>tq', '<cmd>tabc<cr>', { silent = true, desc = 'Close Current Tab' })
+
